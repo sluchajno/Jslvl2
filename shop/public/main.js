fc3201a5a860302e46fb89847e5c266747d92c5e
sluchajno <72516983+sluchajno@users.noreply.github.com>
2020-11-22 22:07:47 +0300

    22.11.2020

--- shop/public/main.js
+++ shop/public/main.js
@@ -231,6 +231,63 @@ class GoodsList extends List {
     }
 }
 
+class ListLoad extends GoodItem {
+    _cartInstance = null
+    _pageCounter = 1
+    constructor(CartInstance) {
+        super()
+        this._cartInstance = CartInstance
+        this.iniShowMoreBtn()
+        let goodsPromise = this.fethGoods()
+        googPromise.then(() => {
+            this.render()
+        })
+    }
+
+    iniShowMoreBtn() {
+        const btn = document.querySelector('.showmore')
+        btn.addEventListerer('click', () => {
+            this.fetchGoods()
+                .then(() => {
+                    this.render()
+                })
+        })
+    }
+    hideShowMoreBtn() {
+        const btn = document.querySelector('.showmore')
+        btn.remove()
+    }
+
+    fetchGoods() {
+        const result = fetch(getMainTemplate)
+        return result
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                this._pageCounter++
+                this.items.push(...data.data.map(cur => {
+                    return new GoodItem(cur, this._cartInstance)
+                }))
+            })
+            .catch(e => {
+                this.hideShowMoreBtn()
+                console.log(e)
+            })
+    }
+
+    render() {
+        const placeToRender = document.querySelector('.card__list')
+        if (placeToRender) {
+            placeToRender.innerHTML = ''
+            this.items.forEach(good => {
+                good.render(placeToRender)
+            })
+        }
+    }
+}
+
+
 const Good1 = new GoodItem('Apple SE 2020', 28000)
 const Good2 = new GoodItem('Apple SE', 10500)
 const Good3 = new GoodItem('Apple 11 256Gb', 60000)
